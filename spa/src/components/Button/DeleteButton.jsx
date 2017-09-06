7cf635891af5dd6693e457c45e0403ae1c89262d
gnerkus <ifeanyioraelosi@gmail.com>
2017-09-06 16:22:01 +0200

    feat(row): define comp for deletes
    The DeleteButton component is used to trigger the removal of a row or column.

---
+++ spa/src/components/Button/DeleteButton.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class DeleteButton extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+  }
+
+  handleButtonClick (clickEvent) {
+    // this.props.handleLabelChange(this.props.labelKey, value)
+  }
+
+  render () {
+    return (
+      <div className='thumbnail-wrap'>
+        <div
+          className='cell-button-label'
+          onClick={this.handleButtonClick}
+        >x</div>
+      </div>
+    )
+  }
+}
+
+export default DeleteButton
