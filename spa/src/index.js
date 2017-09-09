5786e67e309868c409525433f14b44d2a1bd4608
gnerkus <ifeanyioraelosi@gmail.com>
2017-09-09 18:31:06 +0200

    feat(questions): define app routes

--- spa/src/index.js
+++ spa/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
+import Routes from './routes'
 
 ReactDOM.render(
-  <App />,
+  <Routes />,
   document.getElementById('root')
 )
